b6bc5cd06750d30824af602a8823bce4b7877e67
foysalmia <mfoysal314@gmail.com>
2022-07-27 11:56:47 +0600

    bubble sorting added

---
+++ Bubble Sort/Ascending.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print_array(int *a,int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+
+    int a[n];
+    cout<<"Enter elements to the array : ";
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    cout<<"Before Sorting : ";
+    print_array(a,n);
+
+    for(int i=1;i<n;i++){
+        int flag = 0;
+        for(int j=0;j<n-i;j++){
+            if(a[j]>a[j+1]){
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+                flag = 1;
+            }
+        }
+        if(!flag){
+            break;
+        }
+    }
+
+    cout<<"After Bubble Sorting by ascending order : ";
+    print_array(a,n);
+    return 0;
+}
